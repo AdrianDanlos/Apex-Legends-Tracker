a61fab12c953c49bddcd5d0b029a6c042671385f
AdrianDanlos <danlosadrian@gmail.com>
2020-02-12 23:13:48 +0100

    responsive

--- js/funciones.js
+++ js/funciones.js
@@ -6,7 +6,22 @@ $( document ).ready(function() {
     let xhr; //AjaxCall Object
     let refreshAvailable = true;
 
+    'use strict';
 
+    var orientationChange = function () {
+        var $element = $('.selector');
+        $element.css('height', '100vh'); // Change this to your own original vh value.
+        $element.css('height', $element.height() + 'px');
+    };
+
+    var s = screen;
+    var o = s.orientation || s.msOrientation || s.mozOrientation;
+    o.addEventListener('change', function () {
+        setTimeout(function () {
+            orientationChange();
+        }, 250);
+    }, false);
+    orientationChange();
     //Proxies
     //https://secret-ocean-49799.herokuapp.com/
     //https://cors-anywhere.herokuapp.com/
